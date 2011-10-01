8e2f5cf997649cdc5d7ff1e1ed85f400000d9e0b
Arun Kumar Arjunan <arun.immanuel1608@gmail.com>
2011-10-01 22:31:06 +0530

    Do not log to production log

--- config/environments/production.js
+++ config/environments/production.js
@@ -5,7 +5,7 @@ app.configure('production', function () {
     app.enable('merge javascripts');
     app.enable('merge stylesheets');
     app.use(require('express').errorHandler());
-    app.settings.quiet = true;
+    app.settings.quiet = false;
     app.set('ghId',                 '37d2e2b91083bf02ea44');
     app.set('ghSecret',             'af6d41fee211ea2f1c90a7a52a77f8fee99365c6');
     app.set('mongoUrl',             process.env.MONGOHQ_URL);
