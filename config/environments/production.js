bc019a994bdcf414051ce19b2c6161a611f276df
Arun Kumar Arjunan <arun.immanuel1608@gmail.com>
2011-10-01 22:13:01 +0530

    Checking in production.js

--- config/environments/production.js
+++ config/environments/production.js
@@ -8,5 +8,6 @@ app.configure('production', function () {
     app.settings.quiet = true;
     app.set('ghId',                 '37d2e2b91083bf02ea44');
     app.set('ghSecret',             'af6d41fee211ea2f1c90a7a52a77f8fee99365c6');
+    app.set('mongoUrl',             process.env.MONGOHQ_URL);
 });
 
